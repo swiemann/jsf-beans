78eb40008561e3fd2e3e763b7927c92cea387737
swiemann <stefan.wiemann@tu-dresden.de>
2016-07-26 09:45:48 +0200

    initial commit (test)

---
+++ jsf-beans/src/de/tudresden/gis/fusion/beans/data/XMLDocument.java
@@ -0,0 +1,12 @@
+package de.tudresden.gis.fusion.beans.data;
+
+import java.io.Serializable;
+
+public class XMLDocument implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
